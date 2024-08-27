8f0b6cb914adb8bfce6abde49b9d71f37a624d43
vitalizkip <159427584+vitalizkip@users.noreply.github.com>
2024-08-27 14:43:43 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "254717296152"
-global.ownername = "Savage"
+global.ownernomer = "254758479415"
+global.ownername = "Vitaliz"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
